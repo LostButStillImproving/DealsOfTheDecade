7afb3fb2de9aeec2913a6dc511b5a0dae59e9c11
LostButStillImproving <70686136+LostButStillImproving@users.noreply.github.com>
2021-01-09 18:45:38 +0100

    Did some vodoo

--- src/main/java/com/gluonapplication/GluonApplication.java
+++ src/main/java/com/gluonapplication/GluonApplication.java
@@ -2,11 +2,19 @@ package com.gluonapplication;
 
 import com.gluonapplication.views.PrimaryView;
 import com.gluonapplication.views.SecondaryView;
+import com.gluonhq.attach.storage.StorageService;
+import com.gluonhq.attach.util.Services;
+import com.gluonhq.attach.util.impl.ServiceFactory;
 import com.gluonhq.charm.glisten.application.MobileApplication;
 import com.gluonhq.charm.glisten.visual.Swatch;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.Optional;
+
+import static com.sun.javafx.PlatformUtil.*;
 
 public class GluonApplication extends MobileApplication {
 
@@ -30,6 +38,51 @@ public class GluonApplication extends MobileApplication {
     }
 
     public static void main(String args[]) {
+        System.setProperty("javafx.platform", "Desktop");
+
+        // define service for desktop
+        StorageService storageService = new StorageService() {
+            @Override
+            public Optional<File> getPrivateStorage() {
+                // user home app config location (linux: /home/[yourname]/.gluonmaps)
+                return Optional.of(new File(System.getProperty("user.home")));
+            }
+
+            @Override
+            public Optional<File> getPublicStorage(String subdirectory) {
+                // this should work on desktop systems because home path is public
+                return getPrivateStorage();
+            }
+
+            @Override
+            public boolean isExternalStorageWritable() {
+                //noinspection ConstantConditions
+                return getPrivateStorage().get().canWrite();
+            }
+
+            @Override
+            public boolean isExternalStorageReadable() {
+                //noinspection ConstantConditions
+                return getPrivateStorage().get().canRead();
+            }
+        };
+
+        // define service factory for desktop
+        ServiceFactory<StorageService> storageServiceFactory = new ServiceFactory<StorageService>() {
+
+            @Override
+            public Class<StorageService> getServiceType() {
+                return StorageService.class;
+            }
+
+            @Override
+            public Optional<StorageService> getInstance() {
+                return Optional.of(storageService);
+            }
+
+        };
+        // register service
+        Services.registerServiceFactory(storageServiceFactory);
         launch(args);
     }
 }
